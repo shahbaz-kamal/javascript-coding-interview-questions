680cc9a40e872ba2f87d124f13c40f6f43771d72
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2026-01-06 00:25:25 +0600

    solved problem 8: steps

--- exercises/8-steps-string-pattern.ts
+++ exercises/8-steps-string-pattern.ts
@@ -18,11 +18,29 @@
 //       '####'
 
 function steps(n: number) {
+    let char='#'
   for (let i = 1; i <= n; i++) {
+    console.log("'"+char.repeat(i)+" '")
+    // for (let j = 1; j <= i; j++) {
+    //     console.log("'"+char.repeat(j)+" '")
+    // }
+  }
+}
+function steps2(n: number) {
+    let char='#'
+    let lines=""
+    let space=""
+  for (let i = 1; i <= n; i++) {
+    // console.log("'"+char.repeat(i)+" '")
     for (let j = 1; j <= n; j++) {
-        console.log('#')
+        if(j<=i){ lines=lines+char}
+        else lines=lines+" "
+       
     }
+    console.log("'"+lines+"'")
+    lines=""
   }
 }
 
-steps(3);
+// steps(3);
+steps2(6);
