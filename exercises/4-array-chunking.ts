a1303d65b1cf454c2b1d5363f3ca7c067475e7e3
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2026-01-05 22:13:08 +0600

    solved problem 4 (array chunking)

--- exercises/4-array-chunking.ts
+++ exercises/4-array-chunking.ts
@@ -8,4 +8,16 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array:Array<any>, size:number):Array<any> {
+    let result:Array<any>=[]
+let index=0
+
+while(index<array.length){
+    result.push(array.slice(index,index+size))
+    index+=size
+}
+return result
+}
+
+console.log(chunk([1, 2, 3, 4], 2))
+console.log(chunk([1, 2, 3, 4, 5], 4))
