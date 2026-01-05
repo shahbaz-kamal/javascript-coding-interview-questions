13941f324774d11415a9b14987210e02b25a310c
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2026-01-05 21:58:21 +0600

    solved maxChars problem

--- exercises/3-maxchars.ts
+++ exercises/3-maxchars.ts
@@ -5,4 +5,52 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str:string) {}
+function maxChar(str: string): string {
+  let charMap: any = {};
+
+  for (let char of str) {
+    if (charMap[char]) charMap[char] += 1;
+    else charMap[char] = 1;
+  }
+
+//   const [key,value]=Object.entries(charMap)
+
+let max=0
+let maxChar=""
+for (const [key,value] of Object.entries(charMap)){
+    if(Number(value)>max) {
+        max=Number(value)
+        maxChar=key
+    }
+  
+}
+ 
+  return maxChar;
+}
+
+// using for in
+function maxChar1(str: string): string {
+  let charMap: any = {};
+
+  for (let char of str) {
+    if (charMap[char]) charMap[char] += 1;
+    else charMap[char] = 1;
+  }
+
+//   const [key,value]=Object.entries(charMap)
+
+let max=0
+let maxChar=""
+for (let key in charMap){
+if(charMap[key]>max){
+    max=charMap[key]
+    maxChar=key
+}
+  
+}
+ 
+  return maxChar;
+}
+
+console.log(maxChar("abcccccccd"))
+console.log(maxChar1("abcccccaaaaaaaaaaaaaaccd"))
